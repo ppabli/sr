b7d0829e6195499757e124ad5e7def41d151b74f
ppabli <ppabli12@gmail.com>
2019-08-06 22:38:42 +0200

    Se ha implementado la reproducion del texto generado

--- sr.py
+++ sr.py
@@ -1,13 +1,20 @@
 import speech_recognition as sr
+from gtts import *
+import pyglet
+import time, os
 
 r = sr.Recognizer()
 
 with sr.Microphone() as source:
-	print('Di algo')
-	audio = r.listen(source)
-
-try:
-	text = r.recognize_google(audio, language = 'es-ES')
-	print("Has dicho: ", text)
-except:
-	print("Error")
+	print("Di algo")
+	try:
+		audio = r.listen(source)
+		text = r.recognize_google(audio, language = 'es-ES')
+		print("Has dicho:", text)
+		file = gTTS(text = text, lang = 'en', slow = False)
+		filename = './test.mp3'
+		file.save(filename)
+		os.system('afplay  ' + filename)
+		os.remove(filename)
+	except:
+		print("Ha ocurrido un error")
